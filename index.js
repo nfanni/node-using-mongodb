312378a65b0f67058a8205aee08f702871409300
Fanni Nagy <fanni.irisz.nagy@codecool.com>
2022-04-13 13:58:33 +0200

    connect to mongoose

--- index.js
+++ index.js
@@ -1,17 +1,26 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+const mongoose = require('mongoose');
 
 const app = express();
 const PORT = 4000;
+mongoose.connect('mongodb://localhost:27017/productsdb', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+    .catch(e => console.error(e.message))
+    .then(r => console.log(r));
 
 // bodyparser setup
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json()); 
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+
 
 app.get('/', (req, res) =>
     res.send(`Store server running on port ${PORT}`)
 );
 
-app.listen(PORT, () => 
+app.listen(PORT, () =>
     console.log(`Your server is running on port ${PORT}`)
 );
